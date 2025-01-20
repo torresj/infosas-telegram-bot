f4ec3332ca328b1c68f6f4829896bfb1c82a545c
Jaime Torres Benavente <jaime.torres.benavente@apipana.es>
2025-01-20 21:13:36 +0100

    First commit

---
+++ src/main/java/com/torresj/infosas/telegrambot/InfosasTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package com.torresj.infosas.telegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfosasTelegramBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfosasTelegramBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/torresj/infosas/telegrambot/InfosasTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.torresj.infosas.telegrambot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfosasTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
