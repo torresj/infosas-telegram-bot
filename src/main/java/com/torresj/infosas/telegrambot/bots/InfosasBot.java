ec35218e646d2519292796474028c335ffd20c45
Jaime Torres Benavente <jaime.torres.benavente@apipana.es>
2025-01-21 17:32:20 +0100

    first telegram bot working added

---
+++ src/main/java/com/torresj/infosas/telegrambot/bots/InfosasBot.java
@@ -0,0 +1,62 @@
+package com.torresj.infosas.telegrambot.bots;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.longpolling.BotSession;
+import org.telegram.telegrambots.longpolling.interfaces.LongPollingUpdateConsumer;
+import org.telegram.telegrambots.longpolling.starter.AfterBotRegistration;
+import org.telegram.telegrambots.longpolling.starter.SpringLongPollingBot;
+import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.generics.TelegramClient;
+
+
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class InfosasBot implements SpringLongPollingBot, LongPollingSingleThreadUpdateConsumer {
+
+    @Value("${telegram.bot.token}")
+    private final String token;
+
+    private final TelegramClient telegramClient;
+
+    @Override
+    public String getBotToken() {
+        return token;
+    }
+
+    @Override
+    public LongPollingUpdateConsumer getUpdatesConsumer() {
+        return this;
+    }
+
+    @Override
+    public void consume(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            // Set variables
+            String message_text = update.getMessage().getText();
+            long chat_id = update.getMessage().getChatId();
+
+            SendMessage message = SendMessage // Create a message object
+                    .builder()
+                    .chatId(chat_id)
+                    .text(message_text)
+                    .build();
+            try {
+                telegramClient.execute(message); // Sending our message object to user
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @AfterBotRegistration
+    public void afterRegistration(BotSession botSession) {
+        log.info("Registered bot running state is: {}", botSession.isRunning());
+    }
+}
