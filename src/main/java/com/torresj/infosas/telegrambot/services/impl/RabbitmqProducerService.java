d7148a808268bf1edc2f52c88d4cedbddb8b4851
Jaime Torres Benavente <jaime.torres.benavente@apipana.es>
2025-01-24 18:04:06 +0100

    send message updated to send a string

--- src/main/java/com/torresj/infosas/telegrambot/services/impl/RabbitmqProducerService.java
+++ src/main/java/com/torresj/infosas/telegrambot/services/impl/RabbitmqProducerService.java
@@ -1,11 +1,13 @@
 package com.torresj.infosas.telegrambot.services.impl;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.torresj.infosas.telegrambot.models.QueueMessage;
 import com.torresj.infosas.telegrambot.services.ProducerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Service;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 @Slf4j
@@ -13,9 +15,18 @@ import org.springframework.stereotype.Service;
 public class RabbitmqProducerService implements ProducerService {
 
     private final RabbitTemplate rabbitTemplate;
+    private final ObjectMapper objectMapper;
 
     @Override
     public void sendMessage(QueueMessage message) {
-        rabbitTemplate.convertAndSend("commandsExchange",message.getType().name(), message);
+        try {
+            rabbitTemplate.convertAndSend(
+                    "commandsExchange",
+                    message.getType().name(),
+                    objectMapper.writeValueAsString(message)
+            );
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
